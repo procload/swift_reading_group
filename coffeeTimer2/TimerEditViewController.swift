31709f7e10bae82bdffd2c742ac9a74d736c3ef7
procload <ryan@ryanmerrill.net>
2015-05-18 13:25:08 -0400

    first commit

---
+++ coffeeTimer2/TimerEditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimerEditViewController.swift
+//  coffeeTimer2
+//
+//  Created by Ryan Merrill on 5/6/15.
+//  Copyright (c) 2015 procload. All rights reserved.
+//
+
+import UIKit
+
+class TimerEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ coffeeTimer2/TimerModel.swift
@@ -0,0 +1,13 @@
+//
+//  TimerModel.swift
+//  coffeeTimer2
+//
+//  Created by Ryan Merrill on 5/5/15.
+//  Copyright (c) 2015 procload. All rights reserved.
+//
+
+import UIKit
+
+class TimerModel: NSObject {
+   
+}
